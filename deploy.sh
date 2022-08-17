07147751291fd3c2eeb49f04341d9268c0697415
oodake <excel2rlang@gmail.com>
2022-08-18 07:18:07 +0900

    gitignore proxy adress

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-dotfiles=(.zshrc .condarc .zshenv)
+dotfiles=(.zshrc .condarc .zshenv .zshfunc)
 
 for file in "${dotfiles[@]}"; do
 	ln -svf ~/dotfiles/$file ~/
 done
+
+ln -svf ~/dotfiles/.config/nvim ~/.config/nvim
+
