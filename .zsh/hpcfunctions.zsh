d114941134d17e82b3efdf2c8635ade2cfe4e594
oodake <excel2rlang@gmail.com>
2022-08-18 07:17:26 +0900

    zsh refactor

---
+++ .zsh/hpcfunctions.zsh
@@ -0,0 +1,41 @@
+function r-conda () {
+  path_append "$HOME/miniconda3/envs/r4_env/bin"
+  export R_BINARY="$HOME/miniconda3/envs/r4_env/bin/R"
+  export R_HOME="$HOME/miniconda3/envs/r4_env/bin/R"
+}
+
+function r-hpcs01 () {
+  alias R="/opt/R/4.2.0/lib64/R/bin/R"
+  alias radian="radian --r-binary=/opt/R/4.2.0/lib64/R/bin/R"
+}
+
+function killme () {
+  echo "Are you sure to kill all your processes?[y/N]"
+  read str
+  case $str in
+  [Yy])
+    sudo -u oodake kill -SIGKILL -1
+    ;;
+  *)
+    echo "You're going to stay on it. Understood."
+}
+
+psucks () { ps aux | grep oodake | rg $1 }
+ppurge () { psucks $1 | awk '{print $2}' | xargs -I% sudo kill % }
+
+#* proxy
+function useproxy () {
+  if [[ -v PROXY_VAL ]]; then
+    export http_proxy=$PROXY_VAL
+    export https_proxy=$PROXY_VAL
+    export ftp_proxy=$PROXY_VAL
+  fi
+}
+
+function noproxy () {
+  if [[ -v PROXY_VAL ]]; then
+    export http_proxy=
+    export https_proxy=
+    export ftp_proxy=
+  fi
+}
