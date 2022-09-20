b40fad7968ea84a40cd3db6ea920306fcc356ea9
snitch0 <snitch@excel2rlang.com>
2022-09-20 09:13:27 +0900

    added treesitter options

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -56,6 +56,66 @@ return require('packer').startup(
             end,
             -- cond = term,
         })
+        use({
+            'nvim-treesitter/playground',
+            config = function()
+                require "nvim-treesitter.configs".setup {
+                    playground = {
+                        enable = true,
+                        disable = {},
+                        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+                        persist_queries = false, -- Whether the query persists across vim sessions
+                        keybindings = {
+                            toggle_query_editor = 'o',
+                            toggle_hl_groups = 'i',
+                            toggle_injected_languages = 't',
+                            toggle_anonymous_nodes = 'a',
+                            toggle_language_display = 'I',
+                            focus_language = 'f',
+                            unfocus_language = 'F',
+                            update = 'R',
+                            goto_node = '<cr>',
+                            show_help = '?',
+                        },
+                    }
+                }
+            end
+        })
+        use({
+            'nvim-treesitter/nvim-treesitter-textobjects',
+            config = function()
+                require'nvim-treesitter.configs'.setup {
+                    textobjects = {
+                        select = {
+                            enable = true,
+
+                            -- Automatically jump forward to textobj, similar to targets.vim
+                            lookahead = true,
+
+                            keymaps = {
+                                -- You can use the capture groups defined in textobjects.scm
+                                ["af"] = "@function.outer",
+                                ["if"] = "@function.inner",
+                                ["ac"] = "@class.outer",
+                                -- you can optionally set descriptions to the mappings (used in the desc parameter of nvim_buf_set_keymap
+                                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            },
+                            -- You can choose the select mode (default is charwise 'v')
+                            selection_modes = {
+                                ['@parameter.outer'] = 'v', -- charwise
+                                ['@function.outer'] = 'V', -- linewise
+                                ['@class.outer'] = '<c-v>', -- blockwise
+                            },
+                            -- If you set this to `true` (default is `false`) then any textobject is
+                            -- extended to include preceding xor succeeding whitespace. Succeeding
+                            -- whitespace has priority in order to act similarly to eg the built-in
+                            -- `ap`.
+                            include_surrounding_whitespace = true,
+                        },
+                    },
+                }
+            end
+        })
         --
         use({
             'David-Kunz/treesitter-unit',
@@ -385,7 +445,7 @@ augroup END
                 cmp.setup.cmdline('/', {
                     mapping = cmp.mapping.preset.cmdline(),
                     sources = {
-                        { name = 'buffer' }
+                        { name = 'b}uffer' }
                     }
                 })
                 cmp.setup.cmdline(":", {
