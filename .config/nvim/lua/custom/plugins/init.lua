476e2ab55289b496cf3b127a2316d460609f3f78
snitch0 <snitch@excel2rlang.com>
2023-04-10 08:07:41 +0900

    added spider motion

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -265,5 +265,15 @@ return {
       },
     },
     dependencies = { { "nvim-lua/plenary.nvim" } },
+  },
+  {
+    "chrisgrieser/nvim-spider",
+    lazy = true,
+    keys = {
+      {"w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" }},
+      {"e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" }},
+      {"b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" }},
+      {"ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" }}
+    }
   }
 }
