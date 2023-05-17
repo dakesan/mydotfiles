7aad0e57510e63ec38739701eb3b96d8547e46a1
HOdake <26640737+dakesan@users.noreply.github.com>
2023-05-17 10:06:52 +0900

    utilized deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ for file in "${dotfiles[@]}"; do
 	ln -svf $HOME/dotfiles/$file $HOME/
 done
 
+mkdir $HOME/.config
+
 rm -rf $HOME/.config/nvim
 unlink $HOME/.config/nvim
 ln -svf $HOME/dotfiles/.config/nvim $HOME/.config/nvim
@@ -14,6 +16,7 @@ mkdir -p $HOME/.config/procs
 ln -svf $HOME/dotfiles/.config/procs/config.toml $HOME/.config/procs/config.toml
 
 mkdir -p $HOME/.sheldon
+mkdir -p $HOME/.config/sheldon
 ln -svf $HOME/dotfiles/.sheldon/plugins.toml $HOME/.sheldon/plugins.toml
 ln -svf $HOME/dotfiles/.sheldon/plugins.toml $HOME/.config/sheldon/plugins.toml
 
@@ -21,6 +24,7 @@ rm -rf $HOME/.config/zellij
 unlink $HOME/.config/zellij
 ln -svf $HOME/dotfiles/.config/zellij $HOME/.config/zellij
 
+mkdir $HOME/.poshthemes
 ln -svf $HOME/dotfiles/oh-my-posh/snitch_custom.omp.json $HOME/.poshthemes/snitch_custom.omp.json
 ln -svf $HOME/dotfiles/oh-my-posh/snitch_custom_home.omp.json $HOME/.poshthemes/snitch_custom_home.omp.json
 ln -svf $HOME/dotfiles/oh-my-posh/snitch_custom_work.omp.json $HOME/.poshthemes/snitch_custom_work.omp.json
