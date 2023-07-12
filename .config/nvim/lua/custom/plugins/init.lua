961a27adaa1d205299a96ed642d9b0bce960ec88
Ubuntu <ubuntu@ip-172-31-19-183.ap-northeast-1.compute.internal>
2023-07-12 01:59:43 +0000

    otter specification has been changed

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -161,7 +161,7 @@ return {
         'jmbuhr/otter.nvim',
         dev = false,
         config = function()
-          require 'otter.config'.setup {
+          require 'otter'.setup {
             lsp = {
               hover = {
                 border = require 'misc.style'.border
