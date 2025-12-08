232bebd79b3f5dd38ec451e712aff19076935cc4
dakesan <26640737+dakesan@users.noreply.github.com>
2025-12-09 08:29:12 +0900

    20251209

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,2 +1,5 @@
 -- Keymaps
 -- clipboard is already set in settings.lua (vim.opt.clipboard = 'unnamedplus')
+
+-- Save with Command+S (works in GUI Neovim like Neovide)
+vim.keymap.set({ 'n', 'i', 'v' }, '<D-s>', '<Cmd>w<CR>', { desc = 'Save file' })
