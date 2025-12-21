672ae4e0bb03aa4b6fc6281266a0ab02d9b1c548
dakesan <26640737+dakesan@users.noreply.github.com>
2025-12-21 15:01:14 +0900

    feat: add mamba initialization to fish and zsh configurations
    Add mamba shell integration to both fish and zsh shell configurations
    to enable conda/mamba environment management.

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -244,3 +244,10 @@ end
 
 # Added by Antigravity
 fish_add_path /Users/oodakemac/.antigravity/antigravity/bin
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba shell init' !!
+set -gx MAMBA_EXE "/opt/homebrew/opt/micromamba/bin/mamba"
+set -gx MAMBA_ROOT_PREFIX "/Users/oodakemac/mamba"
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
